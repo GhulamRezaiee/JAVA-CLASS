c84a5a7800ed5e06835d26f7d796bdc5f0c1687d
GhulamRezaiee <ghhrezaiee2000@gmail.com>
2020-04-14 19:33:24 -0400

    Last update before class 27

--- src/com/Syntax/Class14/Store.java
+++ src/com/Syntax/Class14/Store.java
@@ -6,13 +6,13 @@ public class Store {
 	public static void main(String[] args) {
 		
 		//create an object from Phone class
-		//instanciate Phone class
+		//Instantiate Phone class
 		
 		//new Phone(); --> create an Object of Phone
-		//iphone --> variable/reference variable
-		// Phone --> type pf varibale
+		//iPhone --> variable/reference variable
+		// Phone --> type of variable
 		Phone iphone = new Phone();
-		iphone.brand = "Iphone";
+		iphone.brand = "iPhone";
 		iphone.color = "White";
 		iphone.model = "X Max";
 		iphone.dial();
