854ede0812ff134f5a1fddeb0109fe5f15a66172
GhulamRezaiee <ghhrezaiee2000@gmail.com>
2020-05-19 21:14:53 -0400

    Final Update for Java Codes

---
+++ src/com/Syntax/Class05/NextvsNextLn.java
@@ -0,0 +1,22 @@
+package com.Syntax.Class05;
+
+import java.util.Scanner;
+
+public class NextvsNextLn {
+
+	public static void main(String[] args) {
+		Scanner variable = new Scanner(System.in);
+
+		System.out.println("Please enter any text");
+		String text1 = variable.next();
+		
+		System.out.println("Please enter any text");
+		String text = variable.nextLine();
+
+		System.out.println(text);
+		System.out.println(text1);
+		
+
+	}
+
+}
